e83e85567e7b49ab048e88c807bf080dc87ba797
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-27 16:36:46 +0300

    completed phase using active model serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,18 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  # If we wanted to use our new custom serializer to render the full collection of movies, we would need to create another route and action
+  # The use of each_serializer: MovieSummarySerializer in our action tells the app to use our custom movie summary serializer to render each of the movies in the collection.
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
   private
 
   def render_not_found_response
